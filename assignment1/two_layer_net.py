ea2cb2ec0709426f7ab95b99d6cc64d3ada4aa30
Jack Munday <jackmunday@jackmunday.local>
2020-12-13 12:11:27 +0000

    Stanford cs231n Assignment1 Final Submission

--- assignment1/two_layer_net.py
+++ assignment1/two_layer_net.py
@@ -1,42 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 # # Implementing a Neural Network
 # In this exercise we will develop a neural network with fully-connected layers to perform classification, and test it out on the CIFAR-10 dataset.
 
-# In[ ]:
-
-
-# A bit of setup
-
 import numpy as np
 import matplotlib.pyplot as plt
-
 from cs231n.classifiers.neural_net import TwoLayerNet
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-
-# for auto-reloading external modules
-# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
 
 def rel_error(x, y):
     """ returns relative error """
     return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
 
-
 # We will use the class `TwoLayerNet` in the file `cs231n/classifiers/neural_net.py` to represent instances of our network. The network parameters are stored in the instance variable `self.params` where keys are string parameter names and values are numpy arrays. Below, we initialize toy data and a toy model that we will use to develop your implementation.
-
-# In[ ]:
-
 
 # Create a small net and some toy data to check your implementations.
 # Note that we set the random seed for repeatable experiments.
-
 input_size = 4
 hidden_size = 10
 num_classes = 3
@@ -55,14 +33,10 @@ def init_toy_data():
 net = init_toy_model()
 X, y = init_toy_data()
 
-
 # # Forward pass: compute scores
 # Open the file `cs231n/classifiers/neural_net.py` and look at the method `TwoLayerNet.loss`. This function is very similar to the loss functions you have written for the SVM and Softmax exercises: It takes the data and weights and computes the class scores, the loss, and the gradients on the parameters. 
 # 
 # Implement the first part of the forward pass which uses the weights and biases to compute the scores for all inputs.
-
-# In[ ]:
-
 
 scores = net.loss(X)
 print('Your scores:')
@@ -82,12 +56,8 @@ print()
 print('Difference between your scores and correct scores:')
 print(np.sum(np.abs(scores - correct_scores)))
 
-
 # # Forward pass: compute loss
 # In the same function, implement the second part that computes the data and regularization loss.
-
-# In[ ]:
-
 
 loss, _ = net.loss(X, y, reg=0.05)
 correct_loss = 1.30378789133
@@ -96,12 +66,8 @@ correct_loss = 1.30378789133
 print('Difference between your loss and correct loss:')
 print(np.sum(np.abs(loss - correct_loss)))
 
-
 # # Backward pass
 # Implement the rest of the function. This will compute the gradient of the loss with respect to the variables `W1`, `b1`, `W2`, and `b2`. Now that you (hopefully!) have a correctly implemented forward pass, you can debug your backward pass using a numeric gradient check:
-
-# In[ ]:
-
 
 from cs231n.gradient_check import eval_numerical_gradient
 
@@ -117,14 +83,10 @@ for param_name in grads:
     param_grad_num = eval_numerical_gradient(f, net.params[param_name], verbose=False)
     print('%s max relative error: %e' % (param_name, rel_error(param_grad_num, grads[param_name])))
 
-
 # # Train the network
 # To train the network we will use stochastic gradient descent (SGD), similar to the SVM and Softmax classifiers. Look at the function `TwoLayerNet.train` and fill in the missing sections to implement the training procedure. This should be very similar to the training procedure you used for the SVM and Softmax classifiers. You will also have to implement `TwoLayerNet.predict`, as the training process periodically performs prediction to keep track of accuracy over time while the network trains.
 # 
 # Once you have implemented the method, run the code below to train a two-layer network on toy data. You should achieve a training loss less than 0.02.
-
-# In[ ]:
-
 
 net = init_toy_model()
 stats = net.train(X, y, X, y,
@@ -138,15 +100,10 @@ plt.plot(stats['loss_history'])
 plt.xlabel('iteration')
 plt.ylabel('training loss')
 plt.title('Training Loss history')
-plt.show()
-
+plt.savefig('ass1_twoLayerNN_LossHistory.png')
 
 # # Load the data
 # Now that you have implemented a two-layer network that passes gradient checks and works on toy data, it's time to load up our favorite CIFAR-10 data so we can use it to train a classifier on a real dataset.
-
-# In[ ]:
-
-
 from cs231n.data_utils import load_CIFAR10
 
 def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000):
@@ -192,7 +149,6 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000):
 
     return X_train, y_train, X_val, y_val, X_test, y_test
 
-
 # Invoke the above function to get our data.
 X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()
 print('Train data shape: ', X_train.shape)
@@ -202,12 +158,8 @@ print('Validation labels shape: ', y_val.shape)
 print('Test data shape: ', X_test.shape)
 print('Test labels shape: ', y_test.shape)
 
-
 # # Train a network
 # To train our network we will use SGD. In addition, we will adjust the learning rate with an exponential learning rate schedule as optimization proceeds; after each epoch, we will reduce the learning rate by multiplying it by a decay rate.
-
-# In[ ]:
-
 
 input_size = 32 * 32 * 3
 hidden_size = 50
@@ -224,16 +176,12 @@ stats = net.train(X_train, y_train, X_val, y_val,
 val_acc = (net.predict(X_val) == y_val).mean()
 print('Validation accuracy: ', val_acc)
 
-
 # # Debug the training
 # With the default parameters we provided above, you should get a validation accuracy of about 0.29 on the validation set. This isn't very good.
 # 
 # One strategy for getting insight into what's wrong is to plot the loss function and the accuracies on the training and validation sets during optimization.
 # 
 # Another strategy is to visualize the weights that were learned in the first layer of the network. In most neural networks trained on visual data, the first layer weights typically show some visible structure when visualized.
-
-# In[ ]:
-
 
 # Plot the loss function and train / validation accuracies
 plt.subplot(2, 1, 1)
@@ -249,25 +197,19 @@ plt.title('Classification accuracy history')
 plt.xlabel('Epoch')
 plt.ylabel('Classification accuracy')
 plt.legend()
-plt.show()
-
-
-# In[ ]:
-
+plt.savefig('ass1_twoLayerNN_acc_intial.png')
 
 from cs231n.vis_utils import visualize_grid
 
 # Visualize the weights of the network
-
 def show_net_weights(net):
     W1 = net.params['W1']
     W1 = W1.reshape(32, 32, 3, -1).transpose(3, 0, 1, 2)
     plt.imshow(visualize_grid(W1, padding=3).astype('uint8'))
     plt.gca().axis('off')
-    plt.show()
+    plt.savefig('ass1_twolayerNN_showWeights.png')
 
 show_net_weights(net)
-
 
 # # Tune your hyperparameters
 # 
@@ -278,14 +220,6 @@ show_net_weights(net)
 # **Approximate results**. You should be aim to achieve a classification accuracy of greater than 48% on the validation set. Our best network gets over 52% on the validation set.
 # 
 # **Experiment**: You goal in this exercise is to get as good of a result on CIFAR-10 as you can (52% could serve as a reference), with a fully-connected Neural Network. Feel free implement your own techniques (e.g. PCA to reduce dimensionality, or adding dropout, or adding features to the solver, etc.).
-
-# **Explain your hyperparameter tuning process below.**
-# 
-# $\color{blue}{\textit Your Answer:}$
-
-# In[ ]:
-
-
 best_net = None # store the best model into this 
 
 #################################################################################
@@ -301,49 +235,69 @@ best_net = None # store the best model into this
 # automatically like we did on the previous exercises.                          #
 #################################################################################
 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+def gen_rand_hyparams (lr_min, lr_max, reg_min, reg_max, h_min, h_max):
+    lr = 10**np.random.uniform(lr_min, lr_max)
+    reg = 10**np.random.uniform(reg_min, reg_max)
+    hidden = np.random.randint(h_min, h_max)
+    return lr, reg, hidden
 
-pass
+def rand_search_hyparams(lr_values, reg_values, h_values):
+    lr = lr_values[np.random.randint(0, len(lr_values))]
+    reg = reg_values[np.random.randint(0, len(reg_values))]
+    hidden = h_values[np.random.randint(0, len(h_values))]
+    return lr, reg, hidden
 
+input_size = 32*32*3
+num_classes = 10
+np.random.seed(0)
+best_val=0
+
+for i in range(20):
+
+    lr, reg, hidden_size = rand_search_hyparams([0.001], [0.05, 0.1, 0.15], [50, 80, 100, 120, 150, 180, 200])
+    net = TwoLayerNet(input_size, hidden_size, num_classes)
+
+    stats =  net.train(X_train, y_train, X_val, y_val, num_iters=2000, batch_size=200, learning_rate=lr, learning_rate_decay=0.95, reg=reg, verbose=False)
+    #predict on training set
+    train_accuracy = (net.predict(X_train)==y_train).mean()
+    #predict on validation set
+    val_accuracy = (net.predict(X_val)==y_val).mean()
+
+    if val_accuracy > best_val:
+        best_val = val_accuracy
+        best_net = net
+        best_stats = stats
+    print('lr %e reg %e hid %d  train accuracy: %f val accuracy: %f' % (lr, reg, hidden_size, train_accuracy, val_accuracy))
+    print('best validation accuracy achieved: %f' % best_val)
 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
+# Plot the loss function and train / validation accuracies
+plt.subplot(2, 1, 1)
+plt.plot(stats['loss_history'])
+plt.title('Loss history')
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
 
-# In[ ]:
-
+plt.subplot(2, 1, 2)
+plt.plot(stats['train_acc_history'], label='train')
+plt.plot(stats['val_acc_history'], label='val')
+plt.title('Classification accuracy history')
+plt.xlabel('Epoch')
+plt.ylabel('Classification accuracy')
+plt.legend()
+plt.savefig('ass1_twoLayerNN_acc_tuned.png')
 
 # Print your validation accuracy: this should be above 48%
 val_acc = (best_net.predict(X_val) == y_val).mean()
 print('Validation accuracy: ', val_acc)
 
-
-# In[ ]:
-
-
 # Visualize the weights of the best network
 show_net_weights(best_net)
-
 
 # # Run on the test set
 # When you are done experimenting, you should evaluate your final trained network on the test set; you should get above 48%.
 
-# In[ ]:
-
-
 # Print your test accuracy: this should be above 48%
 test_acc = (best_net.predict(X_test) == y_test).mean()
 print('Test accuracy: ', test_acc)
-
-
-# **Inline Question**
-# 
-# Now that you have trained a Neural Network classifier, you may find that your testing accuracy is much lower than the training accuracy. In what ways can we decrease this gap? Select all that apply.
-# 
-# 1. Train on a larger dataset.
-# 2. Add more hidden units.
-# 3. Increase the regularization strength.
-# 4. None of the above.
-# 
-# $\color{blue}{\textit Your Answer:}$
-# 
-# $\color{blue}{\textit Your Explanation:}$
-# 
-# 
+#Best accuracy achieved: 0.524
