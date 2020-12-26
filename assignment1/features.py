9f5654475c9da8e325e5731670de2cb77358fc2b
Jack Munday <jackmunday@jackmunday.local>
2020-12-26 07:39:03 +0000

    Stanford cs231n Assignment1 Final Submission

--- assignment1/features.py
+++ assignment1/features.py
@@ -3,44 +3,17 @@
 
 # # Image features exercise
 # *Complete and hand in this completed worksheet (including its outputs and any supporting code outside of the worksheet) with your assignment submission. For more details see the [assignments page](http://vision.stanford.edu/teaching/cs231n/assignments.html) on the course website.*
-# 
 # We have seen that we can achieve reasonable performance on an image classification task by training a linear classifier on the pixels of the input image. In this exercise we will show that we can improve our classification performance by training linear classifiers not on raw pixels but on features that are computed from the raw pixels.
-# 
-# All of your work for this exercise will be done in this notebook.
-
-# In[ ]:
-
 
 import random
 import numpy as np
 from cs231n.data_utils import load_CIFAR10
+import matplotlib; matplotlib.use('agg')
 import matplotlib.pyplot as plt
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-
-# for auto-reloading extenrnal modules
-# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
-
-
-# ## Load data
-# Similar to previous exercises, we will load CIFAR-10 data from disk.
-
-# In[ ]:
-
-
 from cs231n.features import color_histogram_hsv, hog_feature
 
 def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000):
-    # Load the raw CIFAR-10 data
     cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
-
-    # Cleaning up variables to prevent loading data multiple times (which may cause memory issue)
     try:
        del X_train, y_train
        del X_test, y_test
@@ -65,7 +38,6 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000):
 
 X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()
 
-
 # ## Extract Features
 # For each image we will compute a Histogram of Oriented
 # Gradients (HOG) as well as a color histogram using the hue channel in HSV
@@ -83,9 +55,6 @@ X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()
 # function takes a set of images and a list of feature functions and evaluates
 # each feature function on each image, storing the results in a matrix where
 # each column is the concatenation of all feature vectors for a single image.
-
-# In[ ]:
-
 
 from cs231n.features import *
 
@@ -113,24 +82,19 @@ X_train_feats = np.hstack([X_train_feats, np.ones((X_train_feats.shape[0], 1))])
 X_val_feats = np.hstack([X_val_feats, np.ones((X_val_feats.shape[0], 1))])
 X_test_feats = np.hstack([X_test_feats, np.ones((X_test_feats.shape[0], 1))])
 
-
 # ## Train SVM on features
 # Using the multiclass SVM code developed earlier in the assignment, train SVMs on top of the features extracted above; this should achieve better results than training SVMs directly on top of raw pixels.
-
-# In[ ]:
-
-
 # Use the validation set to tune the learning rate and regularization strength
 
 from cs231n.classifiers.linear_classifier import LinearSVM
-
-learning_rates = [1e-9, 1e-8, 1e-7]
-regularization_strengths = [5e4, 5e5, 5e6]
-
+print('\n****     LINEAR SVM    *****')
+#learning_rates = [1e-9, 1e-8, 1e-7, 5e-6, 1e-6]
+#regularization_strengths = [5e4,1e5, 2e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 5e6]
+learning_rates = [1e-3, 1e-2]
+regularization_strengths = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 results = {}
 best_val = -1
 best_svm = None
-
 ################################################################################
 # TODO:                                                                        #
 # Use the validation set to set the learning rate and regularization strength. #
@@ -140,31 +104,39 @@ best_svm = None
 # you should be able to get accuracy of near 0.44 on the validation set.       #
 ################################################################################
 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+def randsearch_hyparams(lr_min, lr_max, reg_min, reg_max, h_min, h_max):
+    lr = 10**np.random.uniform(lr_min,lr_max)
+    reg = 10**np.random.uniform(reg_min,reg_max)
+    hidden = np.random.randint(h_min, h_max)
+    return lr, reg, hidden
+grid_search = [(lr, rg) for lr in learning_rates for rg in regularization_strengths]
+iters = 2000
+for lr, rg in grid_search:
+    svm = LinearSVM()
+    svm.train(X_train_feats, y_train, learning_rate=lr, reg=rg, num_iters=iters)
+    y_train_pred = svm.predict(X_train_feats)
+    train_acc = np.mean(y_train_pred==y_train)
+    y_val_pred = svm.predict(X_val_feats)
+    val_acc = np.mean(y_val_pred==y_val)
 
-pass
+    results[(lr, rg)] = (train_acc, val_acc)
+    if best_val < val_acc:
+        best_val = val_acc
+        best_svm = svm
 
 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
 # Print out results.
 for lr, reg in sorted(results):
     train_accuracy, val_accuracy = results[(lr, reg)]
     print('lr %e reg %e train accuracy: %f val accuracy: %f' % (
                 lr, reg, train_accuracy, val_accuracy))
-    
 print('best validation accuracy achieved during cross-validation: %f' % best_val)
-
-
-# In[ ]:
-
 
 # Evaluate your trained SVM on the test set: you should be able to get at least 0.40
 y_test_pred = best_svm.predict(X_test_feats)
 test_accuracy = np.mean(y_test == y_test_pred)
-print(test_accuracy)
-
-
-# In[ ]:
-
+print('Test accuracy of LinearSVM: %f' % test_accuracy)
+# Best Test accuracy achieved: 0.483
 
 # An important way to gain intuition about how an algorithm works is to
 # visualize the mistakes that it makes. In this visualization, we show examples
@@ -183,48 +155,29 @@ for cls, cls_name in enumerate(classes):
         plt.axis('off')
         if i == 0:
             plt.title(cls_name)
-plt.show()
-
-
-# ### Inline question 1:
-# Describe the misclassification results that you see. Do they make sense?
-# 
-# 
-# $\color{blue}{\textit Your Answer:}$
-# 
-# 
-# 
+plt.savefig('ass1_features_misclassified.png')
 
 # ## Neural Network on image features
 # Earlier in this assigment we saw that training a two-layer neural network on raw pixels achieved better classification performance than linear classifiers on raw pixels. In this notebook we have seen that linear classifiers on image features outperform linear classifiers on raw pixels. 
-# 
 # For completeness, we should also try training a neural network on image features. This approach should outperform all previous approaches: you should easily be able to achieve over 55% classification accuracy on the test set; our best model achieves about 60% classification accuracy.
-
-# In[ ]:
-
 
 # Preprocessing: Remove the bias dimension
 # Make sure to run this cell only ONCE
-print(X_train_feats.shape)
+#print(X_train_feats.shape)
 X_train_feats = X_train_feats[:, :-1]
 X_val_feats = X_val_feats[:, :-1]
 X_test_feats = X_test_feats[:, :-1]
-
-print(X_train_feats.shape)
-
-
-# In[ ]:
-
+#print(X_train_feats.shape)
 
 from cs231n.classifiers.neural_net import TwoLayerNet
-
+print('\n****     TWO LAYER NEURAL NET    ****')
 input_dim = X_train_feats.shape[1]
 hidden_dim = 500
 num_classes = 10
-
 net = TwoLayerNet(input_dim, hidden_dim, num_classes)
 best_net = None
-
+best_val = 0
+best_stats = 0
 ################################################################################
 # TODO: Train a two-layer neural network on image features. You may want to    #
 # cross-validate various parameters as in previous sections. Store your best   #
@@ -232,17 +185,22 @@ best_net = None
 ################################################################################
 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-pass
+for _ in range(20):
+    lr, reg, hidden_size = randsearch_hyparams(-1, 0, -7, -4, 10, 500)
+    net = TwoLayerNet(input_dim, hidden_dim, num_classes)
+    stats = net.train(X_train_feats, y_train, X_val_feats, y_val, num_iters=4000, batch_size = 200, learning_rate = lr, learning_rate_decay=0.95, verbose=False)
+    train_acc = (net.predict(X_train_feats)==y_train).mean()
+    val_acc = (net.predict(X_val_feats)==y_val).mean()
 
-# *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-
-# In[ ]:
-
-
+    if best_val < val_acc:
+        best_val = val_acc
+        best_net = net
+        best_stats = stats
+    print('lr %e reg %e hid %d train acc: %f val acc: %f' % (lr, reg, hidden_size, train_acc, val_acc))
+print('best validation accuracy achieved during cross-validation: %f' % best_val)
 # Run your best neural net classifier on the test set. You should be able
 # to get more than 55% accuracy.
-
 test_acc = (best_net.predict(X_test_feats) == y_test).mean()
-print(test_acc)
+print('Test accuracy of 2-NN: %f' % test_acc)
+# Best test accuracy achieved: 0.587
 
