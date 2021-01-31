1d114cf8d8ad5f90e26defc4906cdcaeae431147
Jack Munday <jackmunday@jackmunday.local>
2021-01-31 13:09:32 +0000

    Final Submission

--- assignment2/Dropout.py
+++ assignment2/Dropout.py
@@ -6,10 +6,6 @@
 # 
 # [1] [Geoffrey E. Hinton et al, "Improving neural networks by preventing co-adaptation of feature detectors", arXiv 2012](https://arxiv.org/abs/1207.0580)
 
-# In[ ]:
-
-
-# As usual, a bit of setup
 from __future__ import print_function
 import time
 import numpy as np
@@ -19,38 +15,16 @@ from cs231n.data_utils import get_CIFAR10_data
 from cs231n.gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
 from cs231n.solver import Solver
 
-get_ipython().magic('matplotlib inline')
-plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-
-# for auto-reloading external modules
-# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
-get_ipython().magic('load_ext autoreload')
-get_ipython().magic('autoreload 2')
-
 def rel_error(x, y):
   """ returns relative error """
   return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
-
-
-# In[ ]:
-
-
-# Load the (preprocessed) CIFAR10 data.
 
 data = get_CIFAR10_data()
 for k, v in data.items():
   print('%s: ' % k, v.shape)
 
-
-# # Dropout forward pass
+print("\nDROPOUT FWD PASS")
 # In the file `cs231n/layers.py`, implement the forward pass for dropout. Since dropout behaves differently during training and testing, make sure to implement the operation for both modes.
-# 
-# Once you have done so, run the cell below to test your implementation.
-
-# In[ ]:
-
 
 np.random.seed(231)
 x = np.random.randn(500, 500) + 10
@@ -67,12 +41,8 @@ for p in [0.25, 0.4, 0.7]:
   print('Fraction of test-time output set to zero: ', (out_test == 0).mean())
   print()
 
-
-# # Dropout backward pass
+print("\nDROPOUT BCKWRD PASS")
 # In the file `cs231n/layers.py`, implement the backward pass for dropout. After doing so, run the following cell to numerically gradient-check your implementation.
-
-# In[ ]:
-
 
 np.random.seed(231)
 x = np.random.randn(10, 10) + 10
@@ -86,19 +56,14 @@ dx_num = eval_numerical_gradient_array(lambda xx: dropout_forward(xx, dropout_pa
 # Error should be around e-10 or less
 print('dx relative error: ', rel_error(dx, dx_num))
 
-
 # ## Inline Question 1:
 # What happens if we do not divide the values being passed through inverse dropout by `p` in the dropout layer? Why does that happen?
 # 
 # ## Answer:
-# [FILL THIS IN]
-# 
+# Dividing values by p ensures that we are calculating the mean and not the sum of all values, without this division there would therefore be an explosion of gradients.
 
-# # Fully-connected nets with Dropout
+print('\nFCNN W/ DROPOUT')
 # In the file `cs231n/classifiers/fc_net.py`, modify your implementation to use dropout. Specifically, if the constructor of the network receives a value that is not 1 for the `dropout` parameter, then the net should add a dropout layer immediately after every ReLU nonlinearity. After doing so, run the following to numerically gradient-check your implementation.
-
-# In[ ]:
-
 
 np.random.seed(231)
 N, D, H1, H2, C = 2, 15, 20, 30, 10
@@ -122,15 +87,11 @@ for dropout in [1, 0.75, 0.5]:
     print('%s relative error: %.2e' % (name, rel_error(grad_num, grads[name])))
   print()
 
-
-# # Regularization experiment
+print("\nREG EXPERIMENT")
 # As an experiment, we will train a pair of two-layer networks on 500 training examples: one will use no dropout, and one will use a keep probability of 0.25. We will then visualize the training and validation accuracies of the two networks over time.
 
-# In[ ]:
-
-
 # Train two identical nets, one with dropout and one without
-np.random.seed(231)
+#np.random.seed(231)
 num_train = 500
 small_data = {
   'X_train': data['X_train'][:num_train],
@@ -156,12 +117,6 @@ for dropout in dropout_choices:
   solvers[dropout] = solver
   print()
 
-
-# In[ ]:
-
-
-# Plot train and validation accuracies of the two models
-
 train_accs = []
 val_accs = []
 for dropout in dropout_choices:
@@ -169,6 +124,7 @@ for dropout in dropout_choices:
   train_accs.append(solver.train_acc_history[-1])
   val_accs.append(solver.val_acc_history[-1])
 
+plt.figure()
 plt.subplot(3, 1, 1)
 for dropout in dropout_choices:
   plt.plot(solvers[dropout].train_acc_history, 'o', label='%.2f dropout' % dropout)
@@ -186,19 +142,17 @@ plt.ylabel('Accuracy')
 plt.legend(ncol=2, loc='lower right')
 
 plt.gcf().set_size_inches(15, 15)
-plt.show()
-
+plt.savefig("q3_Dropout_Train_val_Accuracies.png")
 
 # ## Inline Question 2:
 # Compare the validation and training accuracies with and without dropout -- what do your results suggest about dropout as a regularizer?
 # 
 # ## Answer:
-# [FILL THIS IN]
-# 
+# Dropout simplifies the model through randomly resetting the activation of p % of our neurons, which results in a simpler model that aims to reduce overfitting. When we are not using dropout in the training phase we reach accucaries on the training set of > 90%, whereas with droput the train acc at the same epoch is comparitvely lower at ~70 % - supporting that we are training a simpler model. 
 
 # ## Inline Question 3:
 # Suppose we are training a deep fully-connected network for image classification, with dropout after hidden layers (parameterized by keep probability p). If we are concerned about overfitting, how should we modify p (if at all) when we decide to decrease the size of the hidden layers (that is, the number of nodes in each layer)?
 # 
 # ## Answer:
-# [FILL THIS IN]
-# 
+# p does not need to be modified are is is a probabilty that a neuron will be reset not an absolute number of neurons per layer that gets reset, as this is a probability it is clearly propotional to the layer size and so when adjusting layer size it does not need to be adjusted. 
+
